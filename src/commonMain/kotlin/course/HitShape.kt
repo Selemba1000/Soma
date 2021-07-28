e263471f8695f2e8f2c60da3bcbd643bfedc1370
Selemba <sebastian.lempik@gmx.de>
2021-07-28 22:37:49 +0200

    fixed Indexing errors

--- src/commonMain/kotlin/course/HitShape.kt
+++ src/commonMain/kotlin/course/HitShape.kt
@@ -30,10 +30,10 @@ fun TiledMap.readHitShapes(tilelayer: Int = 0): List<HitShape> {
     val list = emptyList<HitShape>().toMutableList()
 
     data.tilesets.onEach {
-        for (j in 0..this.height) {
-            for (i in 0..this.width) {
-                if (it.tiles[tileLayers[tilelayer][i, j]].objectGroup?.objects?.firstOrNull() != null) {
-                    val obj = it.tiles[tileLayers[tilelayer][i, j]].objectGroup?.objects?.first()
+        for (j in 0 until this.height) {
+            for (i in 0 until this.width) {
+                if (it.tiles.firstOrNull { tile -> tile.id == tileLayers[tilelayer][i, j]-1 }?.objectGroup?.objects?.firstOrNull() != null) {
+                    val obj = it.tiles.first { tile -> tile.id == tileLayers[tilelayer][i, j]-1 }.objectGroup?.objects?.first()
                     when (obj?.objectShape) {
                         is TiledMap.Object.Shape.Rectangle -> list.add(HitShape(i, j, obj.bounds.toPoints()))
                         is TiledMap.Object.Shape.Polygon -> list.add(HitShape(i, j, (obj.objectShape as TiledMap.Object.Shape.Polygon).points))
